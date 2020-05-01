a015dea8ed9983fe63b0feea4398f4edd90ef139
Lenar <lenar.evstafyev@mail.ru>
2020-05-01 23:20:38 +0300

    fixed

--- Async.js
+++ Async.js
@@ -1,18 +1,25 @@
- let getPromise1 = new Promise((resolve) => {
-    resolve(1)
+let getPromise1 = new Promise((resolve) => {
+  resolve(1)
+ 
  })
 let getPromise2 = new Promise((resolve) => {
-  resolve(1)
+  resolve(2)
 })
 
 const all = (promise1, promise2) => {
-  let counter = 0
+    
+let counter = 0
+let data = []
 
-  promise1.then(counter++)
-  promise2.then(counter++)
-
-  return counter >= 2 && Promise.all([promise1, promise2])  
-}
-all(getPromise1, getPromise2).then(value => {
- console.log(value)
+while(counter < 2){
+  promise1.then((value)=> data.push(value), counter++)
+  promise2.then((value)=> data.push(value), counter++) 
+} 
+return new Promise(resolve=>{
+    resolve(data)
 })
+
+
+
+}
+all(getPromise1, getPromise2).then(data => console.log(data))
