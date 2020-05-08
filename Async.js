a191a846bcebb70b42edf80a66d737519112f366
Lenar <lenar.evstafyev@mail.ru>
2020-05-08 21:29:28 +0300

    update file

--- Async.js
+++ Async.js
@@ -1,25 +1,24 @@
 let getPromise1 = new Promise((resolve) => {
-  resolve(1)
- 
- })
+  setTimeout(() => {
+    resolve(1);
+  }, 2000);
+});
 let getPromise2 = new Promise((resolve) => {
-  resolve(2)
-})
+  resolve(2);
+});
 
-const all = (promise1, promise2) => {
-    
-let counter = 0
-let data = []
+const all = ([...pomisesArray]) => {
+  let counter = 0;
+  let data = [];
 
-while(counter < 2){
-  promise1.then((value)=> data.push(value), counter++)
-  promise2.then((value)=> data.push(value), counter++) 
-} 
-return new Promise(resolve=>{
-    resolve(data)
-})
-
-
-
-}
-all(getPromise1, getPromise2).then(data => console.log(data))
+  return new Promise((resolve) => {
+    pomisesArray.forEach((item, idx) => {
+      item.then((value) => {
+        data[idx] = value;
+        counter++;
+        counter == pomisesArray.length && resolve(data);
+      });
+    });
+  });
+};
+all([getPromise1, getPromise2]).then((data) => console.log(data));
